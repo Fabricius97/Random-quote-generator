ab5392cb90fd039432263c59606bc736337571dd
Fabricius97 <christoffer.fabricius@hotmail.com>
2023-03-23 13:51:23 +0100

    Getting api to my eventlistner andtext for my the id number

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const quotes = [
-    "The best way to predict the future is to invent it. - Alan Kay",
-    "A ship in port is safe, but that's not what ships are built for. - Grace Hopper",
-    "I have not failed. I've just found 10,000 ways that won't work. - Thomas Edison",
-    "The only way to do great work is to love what you do. - Steve Jobs",
-    "If you want to go fast, go alone. If you want to go far, go together. - African Proverb",
-    "We are what we repeatedly do. Excellence, then, is not an act, but a habit. - Aristotle",
-    "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
-  ];
+
+ 
+const randomQuoteButton = document.getElementById("random-quote-button");
+const quotesEl = document.getElementById("quote");
+const advideID = document.getElementById("advide-id");
+
+async function getData() {
+    const response = await fetch("https://api.adviceslip.com/advice");
+    const data = await response.json();
+    quotesEl.innerText = data.slip.advice;
+    advideID.innerText = data.slip.id;   
+} 
+
+randomQuoteButton.addEventListener("click", () => {
+    getData();
+})
+
+getData();  
   
-  const quotesEl = document.getElementById("quote");
-  const randomQuoteButton = document.getElementById("random-quote-button");
-  
-  randomQuoteButton.addEventListener("click", function () {
-    const randomIndex = Math.floor(Math.random() * quotes.length);
-    quotesEl.textContent = quotes[randomIndex];
-  });  
